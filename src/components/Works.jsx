a7d75a711931faee580b5be688d336f51e18d5ea
Samuel <torressam333@gmail.com>
2023-11-03 19:43:35 -0700

    Init the motion div and p needed for animated project cards

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,11 +3,34 @@ import { motion } from 'framer-motion';
 import { styles } from '../styles';
 import { github } from '../assets';
 import { SectionWrapper } from '../hoc';
-import { projects } from '../dconstants';
-import { fadeIn, textVariant } from '../motion';
+import { projects } from '../constants';
+import { textVariant, fadeIn } from '../utils/motion';
 
-const Works = () => {
-  return <div>Works</div>;
-};
+const Works = () => (
+  <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Some of my work...</p>
+      <h2 className={styles.sectionHeadText}>Personal and Professional.</h2>
+    </motion.div>
 
-export default SectionWrapper(Works, 'Works');
+    <div className='w-full flex'>
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I have created and worked on a variety of software engineering projects,
+        making major contributions to each. These projects reflect my ability to
+        work across the full stack, help build national and international web
+        applications and also demonstrate genuine passion and commitment to
+        continuous learning in the software engineering field.
+      </motion.p>
+    </div>
+    <div className='mt-20 flex flex-wrap gap-7'>
+      {projects.map((project, index) => (
+        <ProjectCard key={`project-${index}`} {...project} index={index} />
+      ))}
+    </div>
+  </>
+);
+
+export default SectionWrapper(Works, '');
