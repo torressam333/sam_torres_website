e2e9a41e35fc7d78d16344640d127c2ba3329313
Samuel <torressam333@gmail.com>
2023-11-06 11:09:57 -0700

    Add image optimizer plugin for bundle

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,49 +1,37 @@
 import logo from './logo.svg';
 import st3Logo from './st3_logo.svg';
-import backend from './backend.png';
-import creator from './creator.png';
-import mobile from './mobile.png';
-import web from './web.png';
-import github from './github.png';
-import menu from './menu.svg';
+import mobile from './mobile.png?optimized';
+import github from './github.png?optimized';
 import close from './close.svg';
-import saaCo3 from '../assets/saaCo3.png';
-import nodeLogo from '../assets/node.png';
-import vueReactLogo from '../assets/vue-react.png';
-import SDC1 from '../assets/SDC1.png';
-import SDC2 from '../assets/SDC2.png';
-import Goodwill from '../assets/Goodwill_Industries_Logo.png';
-import Palomar from '../assets/Palomar-college.png';
-import sdc_project from './sdc_project.png';
-import mca_project from './mca_project.png';
-import cinemania_project from './cinemania_project.png';
-import privateSnsProject from './private_sns_plugin_tool.png';
-import Laravel from './laravel.png';
+import saaCo3 from '../assets/saaCo3.png?optimized';
+import nodeLogo from '../assets/node.png?optimized';
+import vueReactLogo from '../assets/vue-react.png?optimized';
+import SDC1 from '../assets/SDC1.png?optimized';
+import SDC2 from '../assets/SDC2.png?optimized';
+import Goodwill from '../assets/Goodwill_Industries_Logo.png?optimized';
+import Palomar from '../assets/Palomar-college.png?optimized';
+import sdc_project from './sdc_project.png?optimized';
+import mca_project from './mca_project.png?optimized';
+import cinemania_project from './cinemania_project.png?optimized';
+import privateSnsProject from './private_sns_plugin_tool.png?optimized';
+import Laravel from './laravel.png?optimized';
+import menu from './menu.svg';
 
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import mongodb from './tech/mongodb.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import threejs from './tech/threejs.svg';
-
-import meta from './company/meta.png';
-import shopify from './company/shopify.png';
-import starbucks from './company/starbucks.png';
-import tesla from './company/tesla.png';
-
-import carrent from './carrent.png';
-import jobit from './jobit.png';
-import tripguide from './tripguide.png';
+import css from './tech/css.png?optimized';
+import docker from './tech/docker.png?optimized';
+import figma from './tech/figma.png?optimized';
+import git from './tech/git.png?optimized';
+import html from './tech/html.png?optimized';
+import javascript from './tech/javascript.png?optimized';
+import mongodb from './tech/mongodb.png?optimized';
+import nodejs from './tech/nodejs.png?optimized';
+import reactjs from './tech/reactjs.png?optimized';
+import redux from './tech/redux.png?optimized';
+import tailwind from './tech/tailwind.png?optimized';
+import typescript from './tech/typescript.png?optimized';
 
 export {
+  menu,
   Laravel,
   sdc_project,
   mca_project,
@@ -57,12 +45,8 @@ export {
   nodeLogo,
   saaCo3,
   logo,
-  backend,
-  creator,
   mobile,
-  web,
   github,
-  menu,
   close,
   css,
   docker,
@@ -76,13 +60,5 @@ export {
   redux,
   tailwind,
   typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   st3Logo,
 };
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { imageOptimizerPlugin } from './imageOptimizerPlugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [imageOptimizerPlugin(), react()],
   base: '/sam_torres_website',
 });
